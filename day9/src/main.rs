4ab1abefb1dcdf204a4f545499f3782db1547c10
ViperSniper0501 <michaelabrenner@gmail.com>
2023-01-09 04:13:04 -0500

    Completed day9 part 1

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,14 +1,14 @@
-use std::{fs, io::{self, BufRead}};
+use std::{fs, io::{self, BufRead}, collections::HashSet};
 
+#[derive(Default)]
 struct Day9 {
-
+    steps: Vec<(Direction, i32)>,
 }
 
 impl Day9 {
 
     fn new() -> Self {
-        Self {
-        }
+        Self::default()
     }
 
     fn parse(&mut self) {
@@ -24,11 +24,24 @@ impl Day9 {
                 Err(e) => println!("{e}")
             }
 
+            let (dir, dist) = line_data.split_once(' ').unwrap();
+            let dir = Direction::parse(dir);
+            let dist = dist.parse().unwrap();
+            self.steps.push((dir, dist));
+
         }
     }
 
     fn part1(&self) {
-        println!("Part1: Unsolved");
+
+        let mut snake = Snake::default();
+
+        for (dir, amount) in &self.steps {
+            for _ in 0..*amount {
+                snake.make_move(dir);
+            }
+        }
+        println!("Part1: {}", snake.visited.len());
     }
 
     fn part2(&self) {
@@ -36,6 +49,56 @@ impl Day9 {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+impl Direction {
+    fn parse(s: &str) -> Self {
+        match s {
+            "U" => Self::Up,
+            "D" => Self::Down,
+            "L" => Self::Left,
+            "R" => Self::Right,
+            _ => panic!("invalid direction '{s}'"),
+        }
+    }
+}
+
+#[derive(Default)]
+struct Snake {
+    head: (i32, i32),
+    tail: (i32, i32),
+    visited: HashSet<(i32, i32)>
+}
+
+impl Snake {
+    const DIR: [(i32,i32); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+    fn make_move(&mut self, dir: &Direction) {
+        let delta = Self::DIR[*dir as usize];
+        self.head.0 += delta.0;
+        self.head.1 += delta.1;
+
+        let rowdiff = self.head.0 - self.tail.0;
+        let coldiff = self.head.1 - self.tail.1;
+
+        if rowdiff == 0 && coldiff.abs() > 1 {
+            self.tail.1 += coldiff.signum();
+        } else if coldiff == 0 && rowdiff.abs() > 1 {
+            self.tail.0 += rowdiff.signum();
+        } else if rowdiff.abs() > 1 || coldiff.abs() > 1 {
+            self.tail.0 += rowdiff.signum();
+            self.tail.1 += coldiff.signum();
+        }
+
+        self.visited.insert(self.tail);
+    }
+}
 
 fn main() {
     let mut day = Day9::new();
