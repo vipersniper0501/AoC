7ded4e09bee030374ab03668b02a567a391bbf61
ViperSniper0501 <michaelabrenner@gmail.com>
2023-12-27 19:15:28 -0500

    Completed part 2 of day3

--- AoC_2023/day3/src/main.rs
+++ AoC_2023/day3/src/main.rs
@@ -95,6 +95,56 @@ fn check_for_parts(row: i32, col: i32, data: &mut Box<Vec<Vec<char>>>) -> i32 {
     return found_parts_sum;
 }
 
+fn get_gear_ratios(row: i32, col: i32, data: &mut Box<Vec<Vec<char>>>) -> i32 {
+
+    let mut gear_ratio = 0;
+    const SYMBOL: char = '*';
+    let directions = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),           (0, 1),
+        (1, -1), (1, 0), (1, 1),
+    ];
+    
+    // Helper function to check if a position is valid in the matrix
+    let is_valid_position = |r: i32, c: i32, d: &mut Box<Vec<Vec<char>>>| 
+                                             r >= 0 
+                                             && (r as usize) < d.len()
+                                             && c >= 0 
+                                             && (c as usize) < d[0].len();
+
+    // Helper function to check if a character is a symbol
+    let is_symbol = |ch: &char| &SYMBOL == ch;
+
+    // Closure to check if a character is a digit
+    let is_digit = |ch: char| ch.is_digit(10);
+
+    if !is_symbol(&data[row as usize][col as usize]) {
+        return 0;
+    }
+
+    let mut gears: Vec<i32> = Vec::new();
+
+    for &(dr, dc) in &directions {
+        let new_r = row + dr;
+        let new_c = col + dc;
+        if !is_valid_position(new_r, new_c, data) {
+            continue;
+        }
+        let value = data[new_r as usize][new_c as usize];
+        if is_digit(value) == true {
+             gears.push(find_connected_digits(new_r, new_c, data, &is_symbol));
+        }
+    }
+
+    if gears.len() == 2 {
+        gear_ratio = gears[0] * gears[1];
+    }
+
+
+
+    return gear_ratio;
+
+}
 
 fn part1(input: Vec<Vec<char>>) {
 
@@ -110,17 +160,23 @@ fn part1(input: Vec<Vec<char>>) {
     println!("Part 1: Sum of parts = {}", sum_of_parts);
 }
 
-fn part2() {
+fn part2(input: Vec<Vec<char>>) {
+    let mut sum_of_gear_ratios: i32 = 0;
+    let mut data: Box<Vec<Vec<char>>> = Box::new(input.clone());
 
-    println!("Part 2: ");
+    for x in 0..input.len() {
+        for y in 0..input[0].len() {
+            sum_of_gear_ratios += get_gear_ratios(x as i32, y as i32, &mut data);
+        }
+    }
+
+    println!("Part 2: Sum of gear ratios = {}", sum_of_gear_ratios);
 }
 
 
 fn main() {
     let data = parse();
 
-    // println!("{:#?}", data);
-
     part1(data.clone());
-    // part2(data.clone());
+    part2(data.clone());
 }
