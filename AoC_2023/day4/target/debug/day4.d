d76fcf8db98de8f3b6c12197ecc4210002068f3f
ViperSniper0501 <michaelabrenner@gmail.com>
2023-12-28 11:28:15 -0500

    fixing git

--- AoC_2023/day4/target/debug/day4.d
+++
@@ -1,1 +0,0 @@
-/home/viper/dev/AoC/AoC_2023/day4/target/debug/day4: /home/viper/dev/AoC/AoC_2023/day4/src/main.rs
--- AoC_2023/day4/target/debug/deps/day4-d3191cb862a0e1a4.d
+++
@@ -1,5 +0,0 @@
-/home/viper/dev/AoC/AoC_2023/day4/target/debug/deps/libday4-d3191cb862a0e1a4.rmeta: src/main.rs
-
-/home/viper/dev/AoC/AoC_2023/day4/target/debug/deps/day4-d3191cb862a0e1a4.d: src/main.rs
-
-src/main.rs:
