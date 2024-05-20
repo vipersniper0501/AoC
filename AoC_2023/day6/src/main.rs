9b989c2ce5edf16cf398241c73f9393cdd424bcb
vipersniper0501 <michaelabrenner@gmail.com>
2024-05-19 20:47:05 -0400

    completely parser for day 6 2023

--- AoC_2023/day6/src/main.rs
+++ AoC_2023/day6/src/main.rs
@@ -7,14 +7,16 @@ struct Race {
 }
 
 fn parse(file_name: &str) -> Vec<Race> {
-    let races: Vec<Race> = Vec::new();
+    let mut races: Vec<Race> = Vec::new();
 
     let file = fs::File::open(file_name)
         .expect("No file found");
     let reader = io::BufReader::new(file);
 
-    let mut line = "";
-    reader.read_line(line);
+    let mut times: Vec<i32> = Vec::new();
+    let mut distances: Vec<i32> = Vec::new();
+
+    let mut distances_flag: bool = false;
 
     for line in reader.lines() {
         let mut line_data = String::new();
@@ -23,14 +25,35 @@ fn parse(file_name: &str) -> Vec<Race> {
             Err(e) => eprintln!("{e}")
         }
 
-        let values: Vec<&str> = line_data.split_whitespace().collect();
+        let mut values: Vec<&str> = line_data.split_whitespace().collect();
+        values.remove(0);
 
+        let int_values: Vec<i32> = match values.iter().map(|s| s.parse::<i32>()).collect() {
+            Ok(v) => v,
+            Err(e) => {
+                println!("Failed to parse value {e}");
+                vec![]
+            }
+
+        };
+
+        if distances_flag == false {
+            times = int_values;
+            distances_flag = true;
+        } else {
+            distances = int_values;
+        }
+    }
+
+    for i in 0..times.len() {
+        let race: Race = Race { time: times[i], record: distances[i] };
+        races.push(race);
     }
 
     return races;
 }
 
-fn part1() {
+fn part1(races: &Vec<Race>) {
 
 }
 
@@ -40,6 +63,7 @@ fn part2() {
 
 
 fn main() {
-    parse("data/input");
+    let races: Vec<Race> = parse("data/input");
+    part1(&races);
 
 }
