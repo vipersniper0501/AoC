38410083fea267e9c21d7b269a312d43094d9fa9
vipersniper0501 <viper@michaelbrenner.dev>
2024-08-10 08:39:05 -0400

    stubbed day7 2023

--- AoC_2023/day7/src/main.rs
+++ AoC_2023/day7/src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs, io::{self, BufRead}};
+
+fn parse(file_name: &str) {
+
+    let file = fs::File::open(file_name).expect("No file found");
+    let reader = io::BufReader::new(file);
+    
+    for line in reader.lines() {
+        let mut line_data = String::new();
+        match line {
+            Ok(v) => line_data = v,
+            Err(e) => eprintln!("{e}")
+        }
+    }
+
+}
+
+fn day1() {
+
+}
+
+fn day2() {
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
